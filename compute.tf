01ceaf643c86f0ec6b35d689865dbb784efd883f
eros <erosleal@outlook.es>
2026-09-18 22:07:17 -0500

    remote state ec2 lab

--- compute.tf
+++ compute.tf
@@ -1,11 +1,7 @@
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
+  name_regex  = "^amzn2-ami-hvm-.*-x86_64-gp2$"
 }
 
 resource "aws_instance" "app" {
